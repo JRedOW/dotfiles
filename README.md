25c8a66e1c6273d9743446587c963a35b77ebb2b
JRed <joseph@titaniumcoast.com>
2022-02-03 20:56:36 -0500

    Why?

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Dotfiles
+
+Why?
+
+## Stuff
+
+- gitpod gpg
+  - For gitpod, makes it so gitpod signs commits.
---
+++ scripts/gpgSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[[ ! -z $GNUPG_KEY  ]] &&
+
+[[ ! -z $GNUPG_KEYID  ]] &&
+
+gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d)
+
+echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
+
+git config --global user.signingkey $GNUPG_KEYID
+
+git config --global commit.gpgsign true
