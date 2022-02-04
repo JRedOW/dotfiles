ed4318a25dac9b2e3c967a9c4d4702e5daeb4caa
JRed <joseph@titaniumcoast.com>
2022-02-03 21:58:25 -0500

    Tired of This BS

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-./scripts/gpgSetup.sh
+gpg () {
+   [[ ! -z $GNUPG_KEY  ]] &&
+
+    [[ ! -z $GNUPG_KEYID  ]] &&
+
+    gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d) &&
+
+    echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf  &&
+
+    git config --global user.signingkey $GNUPG_KEYID &&
+
+    git config --global commit.gpgsign true
+}
+
+gpg
